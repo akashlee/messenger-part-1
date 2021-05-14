12079cc76cc4cf51963acfd3b374cf7ae5f74ee0
Maniganda Prakash Kannan <manigandaprakash.kannan@synchronoss.com>
2021-05-13 20:48:29 -0700

    First commit

---
+++ src/main/java/com/message/MessengerPart1Application.java
@@ -0,0 +1,13 @@
+package com.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerPart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/message/MessengerPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerPart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
